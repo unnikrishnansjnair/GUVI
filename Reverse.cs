9d02f1c7ccaa2daf41c34c4813ff2f9b0db687fe
unnikrishnansjnair <47817448+unnikrishnansjnair@users.noreply.github.com>
2019-02-21 12:34:40 +0530

    Add files via upload

---
+++ Reverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GUVI
+{
+    class Reverse
+    {
+        static void Main(string[] args)
+        {
+            Reverse rev = new Reverse();
+            Console.Write("Enter a String to reverse : ");
+            string str = Console.ReadLine();
+            rev.check(str);
+        }
+
+        private void check(string s)
+        {
+            char[] chAr = s.ToCharArray();
+            Array.Reverse(chAr);
+            Console.Write("Output : ");
+            Console.WriteLine(chAr);
+            Console.ReadLine();
+        }
+    }
+}
