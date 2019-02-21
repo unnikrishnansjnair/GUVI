e02c30957d82320886bf6c90a06e0019693304ed
unnikrishnansjnair <47817448+unnikrishnansjnair@users.noreply.github.com>
2019-02-21 11:58:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GUVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program num = new Program();
+            Console.Write("Enter the number to check: ");
+            int n=int.Parse(Console.ReadLine());
+            num.check(n);
+        }
+
+        private void check(int n)
+        {
+            
+            if (n > 0)
+            {
+                Console.WriteLine("{0} is a positive Number", n);
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine("{0} is a negative Number", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a nutral Number", n);
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
