6462ca927c80a021f3e104eee3f77a9225c15c69
unnikrishnansjnair <47817448+unnikrishnansjnair@users.noreply.github.com>
2019-02-21 12:20:09 +0530

    Add files via upload

---
+++ OddOrEven.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GUVI
+{
+    class OddOrEven
+    {
+        static void Main(string[] args)
+        {
+            OddOrEven num = new OddOrEven();
+            Console.Write("Enter the number to check: ");
+            int n;
+            bool parsedSuccessfully = int.TryParse(Console.ReadLine(), out n);
+            if (parsedSuccessfully == false)
+            {
+                Console.Write("Please type a number.! Enter the number to check: ");
+                n = int.Parse(Console.ReadLine());
+                num.check(n);
+            }
+            else
+            {
+                num.check(n);
+            }
+            
+        }
+
+        private void check(int n)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine(n+" is an even number.");
+            }
+            else
+            {
+                Console.WriteLine(n + " is an odd number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
